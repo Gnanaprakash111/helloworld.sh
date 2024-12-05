f65829313491cfc4da9bfee60bfa80eacd53a01c
gnana <gnana@gmail.com>
2024-12-05 20:35:27 +0530

    added variables script

---
+++ variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PERSON1=Gnana
+PERSON2=Prakash
+
+echo "$PERSON1: hello $PERSON2, what are you doing"
+
+echo "$PERSON2: hi, $person1, I'm doing work, what about you"
+
+echo "$PERSON1: okay,mee too $PERSON2"
+
+echo "$PERSON2: Carryon, $PERSON1"
+
+
