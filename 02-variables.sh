bffad763de24465e4938fcf6a7f2bfd0638266c2
gnana <gnana@gmail.com>
2024-12-05 20:54:06 +0530

    added another variable files

---
+++ 02-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+DATE=$(date)
+
+echo "script started executing: $DATE"
+
+echo "for this $DATE jenkins will automatically trigger a new job"
---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DATE=$(date)
+
+echo "im created a new job on $DATE"
+
+echo "i have been reached home at $DATE"
+
